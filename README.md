6d2e5bda5ca4be8071f9ee740e480050aca85baf
cxhZCM123 <931736813@qq.com>
2020-02-17 15:17:05 +0800

    修改README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# license_plate_segment
-车牌分割
+# 车牌分割
+利用数字图像处理的方法，依赖python的open-cv库实现的车牌分割
+
+通过修改路径读取包含车牌的图片
+
+```python
+car = cv.imread(local_path, 1)
+```
+
+通过修改HSV颜色空间中的阈值分割不同颜色的车牌
+
+```python
+lower_blue = np.array([100, 110, 110])
+upper_blue = np.array([130, 255, 255])
+lower_yellow = np.array([15, 55, 55])
+upper_yellow = np.array([50, 255, 255]) #预置的蓝色和黄色阈值
+```
+
+显示结果
+
+```python
+cv.imshow('segment', segment)
+```
+
--- segment.py
+++ segment.py
@@ -2,14 +2,15 @@ import cv2 as cv
 import numpy as np
 
 # 输入图片路径
-car = cv.imread('images/canon/IMG_0175.JPG', 1)
+car = cv.imread('images/hp recorder/IMG_009.JPG', 1)
+
 cv.imshow('car', car)
 
 # 设置不同种类车牌的颜色阈值
-lower_blue = np.array([100, 110, 110])
-upper_blue = np.array([130, 255, 255])
-lower_yellow = np.array([15, 55, 55])
-upper_yellow = np.array([50, 255, 255])
+lower_blue = np.array([122, 50, 50])
+upper_blue = np.array([130, 70, 90])
+lower_yellow = np.array([22, 90, 55])
+upper_yellow = np.array([112, 190, 100])
 lower_white = np.array([0, 0, 205])
 upper_white = np.array([50, 60, 255])
 
@@ -20,7 +21,7 @@ mask_yellow = cv.inRange(car_hsv, lower_yellow, upper_yellow)
 mask_white = cv.inRange(car_hsv, lower_white, upper_white)
 
 # 将不同颜色作为分割阈值
-mask_plate = cv.bitwise_and(car_hsv, car_hsv, mask=mask_white)
+mask_plate = cv.bitwise_and(car_hsv, car_hsv, mask=mask_blue+mask_yellow)
 mask_gray = cv.cvtColor(mask_plate, cv.COLOR_BGR2GRAY)
 
 # 利用形态学运算及二值分割提取车牌区域
@@ -32,7 +33,7 @@ ret, mask = cv.threshold(mask_open, 0, 255, cv.THRESH_BINARY)
 # 绘制分割边缘
 area_list = []
 # 设置车牌大小的左界和右界
-area_range = [10000, 50000]
+area_range = [500, 10000]
 contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
 for i in range(len(contours)):
@@ -41,5 +42,7 @@ for i in range(len(contours)):
     if area_range[0] < area < area_range[1]:
         area_list.append(cnt)
 segment = cv.drawContours(car, area_list, -1, (0, 255, 0), 2)
+segment = cv.resize(segment, (1000, 600))
 cv.imshow('segment', segment)
+
 cv.waitKey(0)
